e624ca5074da19fd7a06672b835060ace994ca6a
Jeongdasilver <alal8272@gmail.com>
2018-10-08 00:28:53 +0900

    UPDATE MainActivity ImageView ClickListener

--- HalliGalliCupsAndroid/app/src/main/java/com/de/sh/gram/halligallicupsandroid/MainActivity.kt
+++ HalliGalliCupsAndroid/app/src/main/java/com/de/sh/gram/halligallicupsandroid/MainActivity.kt
@@ -8,9 +8,12 @@ import android.view.MotionEvent.ACTION_DOWN
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.ClipDescription
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.DragEvent
 import android.view.View.*
+import android.widget.ImageView
+import com.de.sh.gram.halligallicupsandroid.R.drawable.red_cup
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,9 +23,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         img_main_red_cup.tag = "IMAGEVIEW_TAG"
-        img_main_green_cup.tag="IMAGEVIEW_TAG"
-        img_main_blue_cup.tag="IMAGEVIEW_TAG"
-        img_main_black_cup.tag="IMAGEVIEW_TAG"
+        img_main_green_cup.tag = "IMAGEVIEW_TAG"
+        img_main_blue_cup.tag = "IMAGEVIEW_TAG"
+        img_main_black_cup.tag = "IMAGEVIEW_TAG"
 
         img_main_red_cup.setOnTouchListener(TouchListener())
         img_main_green_cup.setOnTouchListener(TouchListener())
@@ -31,9 +34,13 @@ class MainActivity : AppCompatActivity() {
 
         layout.setOnDragListener(DragListener())
         img_main_first_cup.setOnDragListener(DragListener())
+        img_main_first_cup.setOnClickListener { imageCheck(img_main_first_cup) }
         img_main_second_cup.setOnDragListener(DragListener())
+        img_main_second_cup.setOnClickListener { imageCheck(img_main_second_cup) }
         img_main_third_cup.setOnDragListener(DragListener())
+        img_main_third_cup.setOnClickListener { imageCheck(img_main_third_cup) }
         img_main_fourth_cup.setOnDragListener(DragListener())
+        img_main_fourth_cup.setOnClickListener { imageCheck(img_main_fourth_cup) }
 
     }
 
@@ -44,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                         view.tag as CharSequence)
 
                 val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-                val data = ClipData(view.tag.toString(),mimeTypes, item)
+                val data = ClipData(view.tag.toString(), mimeTypes, item)
                 val shadowBuilder = View.DragShadowBuilder(view)
 
                 view.startDrag(data, shadowBuilder, view, 0)
@@ -65,22 +72,56 @@ class MainActivity : AppCompatActivity() {
                 DragEvent.ACTION_DROP -> {
                     Log.d("DragClickListener", "ACTION_DROP")
 
-                    if(v.id == R.id.img_main_first_cup || v.id==R.id.img_main_second_cup || v.id==R.id.img_main_third_cup || v.id==R.id.img_main_fourth_cup) {
+                    if (v.id == R.id.img_main_first_cup || v.id == R.id.img_main_second_cup || v.id == R.id.img_main_third_cup || v.id == R.id.img_main_fourth_cup) {
                         val view: View = event.localState as View
                         view.visibility = View.INVISIBLE
                         when (view.id) {
-                            R.id.img_main_red_cup -> v.setBackgroundResource(R.drawable.red_cup)
-                            R.id.img_main_green_cup -> v.setBackgroundResource(R.drawable.green_cup)
-                            R.id.img_main_blue_cup -> v.setBackgroundResource(R.drawable.blue_cup)
-                            R.id.img_main_black_cup -> v.setBackgroundResource(R.drawable.black_cup)
+                            R.id.img_main_red_cup -> {
+                                v.setBackgroundResource(R.drawable.red_cup)
+                                v.tag = R.drawable.red_cup
+                            }
+                            R.id.img_main_green_cup -> {
+                                v.setBackgroundResource(R.drawable.green_cup)
+                                v.tag = R.drawable.green_cup
+                            }
+                            R.id.img_main_blue_cup -> {
+                                v.setBackgroundResource(R.drawable.blue_cup)
+                                v.tag = R.drawable.blue_cup
+                            }
+                            R.id.img_main_black_cup -> {
+                                v.setBackgroundResource(R.drawable.black_cup)
+                                v.tag = R.drawable.black_cup
+                            }
                         }
-                    } else if(v.id == R.id.layout) {
+                    } else if (v.id == R.id.layout) {
                         val view: View = event.localState as View
                         view.visibility = VISIBLE
                     }
                 }
             }
             return true
+        }
+    }
+
+    fun imageCheck(v: View) {
+        when (v.tag) {
+            R.drawable.red_cup -> {
+                img_main_red_cup.visibility = VISIBLE
+                v.background = resources.getDrawable(R.drawable.empty_image_background)
+                v.setBackgroundResource(R.drawable.empty_image_background)
+            }
+            R.drawable.green_cup -> {
+                img_main_green_cup.visibility = VISIBLE
+                v.setBackgroundResource(R.drawable.empty_image_background)
+            }
+            R.drawable.blue_cup -> {
+                img_main_blue_cup.visibility = VISIBLE
+                v.setBackgroundResource(R.drawable.empty_image_background)
+            }
+            R.drawable.black_cup -> {
+                img_main_black_cup.visibility = VISIBLE
+                v.setBackgroundResource(R.drawable.empty_image_background)
+            }
         }
     }
 }
